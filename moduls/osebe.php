651bf462345c726673c476911e66916da4ec02ee
said <alex5bulkyn@gmail.com>
2023-04-10 19:21:44 +0000

    fix foto

--- moduls/osebe.php
+++ moduls/osebe.php
@@ -1,5 +1,5 @@
    <div class="contMain">
-        <section class="myfoto"> <img src="../foto/s68f08qMGAo.jpg" alt="ss" class="f2"> </section>
+        <section class="myfoto"> <img src="foto/s68f08qMGAo.jpg" alt="ss" class="f2"> </section>
         <section class="osebeGrid">
             <h1 class="h1Grid">Саид Шарафмаль</h1>
             <section class="osebe">Всем привет!<br> Мне 23 года,студент МГТУ(заочник),веду активный образ жизни ,люблю ездить в другие города,занимаюсь спортом,услекаюсь моделизмом,нравится програмирование,хочу стать back-end разработчиком в компании Факт.</section>
